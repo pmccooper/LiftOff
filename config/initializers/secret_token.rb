210d987149962160a28e1db366ba85144b28ff6c
Philip Cooper <pmccooper@hotmail.com>
2015-04-19 14:30:47 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiftOff::Application.config.secret_token = 'caf63eb0fd3da01d008fb121a71eceb24a4eb58decf8976b0f707a4208394d37f79010940b2e1e72320a9298eab43f55ed17abba21b4138795b0f0258d9bee8c'
